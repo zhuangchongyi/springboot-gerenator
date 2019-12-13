0543f1f7acd37415140f843536a2e9932667892e
zcy <1909017815@qq.com>
2019-12-13 18:39:50 +0800

    springboot-thymeleaf

--- src/main/java/com/zcy/controller/HelloController.java
+++ src/main/java/com/zcy/controller/HelloController.java
@@ -5,25 +5,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
- * <p>
- *  前端控制器
- * </p>
- *
  * @author zhuangcy
  * @since 2019-12-13
  */
 @Controller
-@RequestMapping("/hello/")
 public class HelloController {
 
     @ResponseBody
     @RequestMapping("hi")
     public String hi(){
-        return "hello";
+        return "hi";
     }
 
+    @RequestMapping("/login")
+    public String index() {
+        return "redirect:hello.html";
+    }
+
+    @RequestMapping("hello/index")
+    public ModelAndView templatesIndex(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        modelAndView.addObject("name", "SpringBoot");
+        return modelAndView;
+    }
+    @RequestMapping("hello")
+    public ModelAndView templatesHello(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("hello");
+        modelAndView.addObject("name", "SpringBoot");
+        return modelAndView;
+    }
 
 }
 
