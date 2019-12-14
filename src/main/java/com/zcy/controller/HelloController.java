11d0c10321dea5662144460a46a3d44e177885fd
zcy <1909017815@qq.com>
2019-12-14 09:06:56 +0800

    springboot-thymeleaf

--- src/main/java/com/zcy/controller/HelloController.java
+++ src/main/java/com/zcy/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.zcy.controller;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -16,26 +17,28 @@ public class HelloController {
 
     @ResponseBody
     @RequestMapping("hi")
-    public String hi(){
+    public String hi() {
         return "hi";
     }
 
-    @RequestMapping("/login")
+    @RequestMapping("login")
     public String index() {
-        return "redirect:hello.html";
+        return "redirect:hello/index";
     }
 
     @RequestMapping("hello/index")
-    public ModelAndView templatesIndex(){
+    public ModelAndView templatesIndex() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("index");
         modelAndView.addObject("name", "SpringBoot");
         return modelAndView;
     }
-    @RequestMapping("hello")
-    public ModelAndView templatesHello(){
+
+    @RequestMapping("hello/{param}")
+    public ModelAndView templatesHello(@PathVariable String param) {
+        System.out.println(param);
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("hello");
+        modelAndView.setViewName("hello/" + param);
         modelAndView.addObject("name", "SpringBoot");
         return modelAndView;
     }
