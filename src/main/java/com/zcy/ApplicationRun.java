f18ef1c8ece5045c5b4ed6d393ba0d0507b3fe5d
zcy <1909017815@qq.com>
2019-12-21 16:18:19 +0800

    springboot-vm

--- src/main/java/com/zcy/ApplicationRun.java
+++ src/main/java/com/zcy/ApplicationRun.java
@@ -1,12 +1,36 @@
 package com.zcy;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@MapperScan("com.zcy.dao")
 public class ApplicationRun {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationRun.class,args);
+        SpringApplication.run(ApplicationRun.class, args);
     }
 
+    /**
+     * SQL输出拦截器
+     */
+    @Bean
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        //sql格式化
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
+    }
+
+
+    /**
+     * 分页查询拦截器
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
 }
